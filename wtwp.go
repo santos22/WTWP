c550243811bf7e6e16e219bd2a7fc8b8106f09e6
Santos <santosjavier22@gmail.com>
2017-07-27 21:12:49 -0700

    Receiving SMS from Twilio #

--- wtwp.go
+++ wtwp.go
@@ -1,16 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/url"
-	"fmt"
 	"strings"
 )
 
 func main() {
-	accountSid := "TWILIO_ACCOUNT_SID"
-	authToken := "TWILIO_AUTH_TOKEN"
+	accountSid := ""
+	authToken := ""
 	urlStr := "https://api.twilio.com/2010-04-01/Accounts/" + accountSid + "/Messages.json"
-	//
+
+	v := url.Values{}
+	v.Set("To", "")
+	v.Set("From", "+")
+	v.Set("Body", "Password is...")
+	rb := *strings.NewReader(v.Encode())
+
+	client := &http.Client{}
+
+	req, _ := http.NewRequest("POST", urlStr, &rb)
+	req.SetBasicAuth(accountSid, authToken)
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	// Send request to Twilio API
+	resp, _ := client.Do(req)
+	fmt.Println(resp.Status)
 
 }
