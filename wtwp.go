23d9997966fa745f6cb61e0fac183d717fbf6bb2
Solorzano <ssolorzano@MTVL159c36361.local>
2017-07-27 20:45:52 -0700

    Initial commit

---
+++ wtwp.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+	"net/url"
+	"fmt"
+	"strings"
+)
+
+func main() {
+	accountSid := "TWILIO_ACCOUNT_SID"
+	authToken := "TWILIO_AUTH_TOKEN"
+	urlStr := "https://api.twilio.com/2010-04-01/Accounts/" + accountSid + "/Messages.json"
+	
+}
